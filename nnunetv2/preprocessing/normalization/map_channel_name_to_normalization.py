622334b3ff2ca6bf37da6e066104b372cfe3cfb4
Ben <ben.orkild@utah.edu>
2026-01-15 10:23:48 -0700

    uncommented print statement to get explicit confirmation of normalization being used for each channel

--- nnunetv2/preprocessing/normalization/map_channel_name_to_normalization.py
+++ nnunetv2/preprocessing/normalization/map_channel_name_to_normalization.py
@@ -20,5 +20,5 @@ def get_normalization_scheme(channel_name: str) -> Type[ImageNormalization]:
     norm_scheme = channel_name_to_normalization_mapping.get(channel_name.casefold())
     if norm_scheme is None:
         norm_scheme = ZScoreNormalization
-    # print('Using %s for image normalization' % norm_scheme.__name__)
+    print('Using %s for image normalization' % norm_scheme.__name__)
     return norm_scheme
