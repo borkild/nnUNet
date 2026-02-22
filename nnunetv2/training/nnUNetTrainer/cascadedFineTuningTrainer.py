9d644b596b0a8f86227e253b6697c556cc7f3699
borkild <ben.orkild@utah.edu>
2026-02-22 13:18:37 -0700

    updated hyperparameters, LR 1e-4 -> 1e-3, Deep Supervision False -> True

--- nnunetv2/training/nnUNetTrainer/cascadedFineTuningTrainer.py
+++ nnunetv2/training/nnUNetTrainer/cascadedFineTuningTrainer.py
@@ -135,7 +135,7 @@ class cascadednnUNetTrainer(nnUNetTrainer):
                 if self.is_cascaded else None
 
         ### Some hyperparameters for you to fiddle with -- for now have deep supervision off and reduced initial LR
-        self.initial_lr = 1e-4
+        self.initial_lr = 1e-3
         self.weight_decay = 3e-5
         self.oversample_foreground_percent = 0.33
         self.probabilistic_oversampling = False
@@ -143,7 +143,7 @@ class cascadednnUNetTrainer(nnUNetTrainer):
         self.num_val_iterations_per_epoch = 50
         self.num_epochs = 1000
         self.current_epoch = 0
-        self.enable_deep_supervision = False
+        self.enable_deep_supervision = True
 
         ### Dealing with labels/regions
         self.label_manager = self.plans_manager.get_label_manager(dataset_json)
