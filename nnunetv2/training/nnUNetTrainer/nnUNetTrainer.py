01e24528745b030bdf90d053628360df7cd58641
borkild <ben.orkild@utah.edu>
2026-02-25 15:28:39 -0700

    lowered initial learning rate 1e-2 -> 1e-3

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -142,7 +142,7 @@ class nnUNetTrainer(object):
                 if self.is_cascaded else None
 
         ### Some hyperparameters for you to fiddle with
-        self.initial_lr = 1e-2
+        self.initial_lr = 1e-3 # originally 1e-2
         self.weight_decay = 3e-3 # originally 3e-5
         self.oversample_foreground_percent = 0.33
         self.probabilistic_oversampling = False
