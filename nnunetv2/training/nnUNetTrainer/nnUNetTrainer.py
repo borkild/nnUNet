753735cce21cfe03704e36d84e5e9736f0b78b69
borkild <ben.orkild@utah.edu>
2026-02-25 18:30:56 -0700

    updated hyperparams: LR: 1e-3 -> 1e-2, num_epochs: 200 -> 400

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -142,13 +142,13 @@ class nnUNetTrainer(object):
                 if self.is_cascaded else None
 
         ### Some hyperparameters for you to fiddle with
-        self.initial_lr = 1e-3 # originally 1e-2
+        self.initial_lr = 1e-2 # originally 1e-2
         self.weight_decay = 3e-2 # originally 3e-5
         self.oversample_foreground_percent = 0.33
         self.probabilistic_oversampling = False
         self.num_iterations_per_epoch = 250
         self.num_val_iterations_per_epoch = 50
-        self.num_epochs = 200 # originally 1000
+        self.num_epochs = 400 # originally 1000
         self.current_epoch = 0
         self.enable_deep_supervision = True
 
