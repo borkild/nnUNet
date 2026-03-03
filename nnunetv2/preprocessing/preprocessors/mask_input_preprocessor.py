2730d0ad34a7ade5f9c7c544e338576f3917ef69
borkild <ben.orkild@utah.edu>
2026-03-03 15:30:41 -0700

    added preprocessor to handle datasets with masks as inputs

---
+++ nnunetv2/preprocessing/preprocessors/mask_input_preprocessor.py
@@ -0,0 +1,190 @@
+#    Copyright 2020 Division of Medical Image Computing, German Cancer Research Center (DKFZ), Heidelberg, Germany
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+import math
+import multiprocessing
+import shutil
+from time import sleep
+from typing import Tuple
+
+import SimpleITK
+import numpy as np
+import pandas as pd
+from batchgenerators.utilities.file_and_folder_operations import *
+from tqdm import tqdm
+from typing import Union
+
+import nnunetv2
+from nnunetv2.paths import nnUNet_preprocessed, nnUNet_raw
+from nnunetv2.preprocessing.cropping.cropping import crop_to_nonzero
+from nnunetv2.preprocessing.resampling.default_resampling import compute_new_shape
+from nnunetv2.training.dataloading.nnunet_dataset import nnUNetDatasetBlosc2
+from nnunetv2.utilities.dataset_name_id_conversion import maybe_convert_to_dataset_name
+from nnunetv2.utilities.find_class_by_name import recursive_find_python_class
+from nnunetv2.utilities.plans_handling.plans_handler import PlansManager, ConfigurationManager
+from nnunetv2.utilities.utils import get_filenames_of_train_images_and_targets
+
+from nnunetv2.preprocessing.preprocessors.default_preprocessor import DefaultPreprocessor
+
+# custom preprocessor that handles inputs that are segmentation masks properly
+# child of default preprocessor, as we use much of the default preprocessor functionality, only changing 1 function
+class maskInputPreprocessor(DefaultPreprocessor):
+    def __init__(self, verbose: bool = True):
+        self.verbose = verbose
+        """
+        Everything we need is in the plans. Those are given when run() is called
+        """
+
+    # this is the only function we change -- get it to handle masks in our array
+    # for now -- we assume that masked data will have the "noNormalization" handle in plan_file->configuration -> normalization_schemes
+    def run_case_npy(self, data: np.ndarray, seg: Union[np.ndarray, None], properties: dict,
+                     plans_manager: PlansManager, configuration_manager: ConfigurationManager,
+                     dataset_json: Union[dict, str]):
+        print("Running run_case_npy in mask preprocessor!!")
+        # let's not mess up the inputs!
+        data = data.astype(np.float32)  # this creates a copy
+        if seg is not None:
+            assert data.shape[1:] == seg.shape[1:], "Shape mismatch between image and segmentation. Please fix your dataset and make use of the --verify_dataset_integrity flag to ensure everything is correct"
+            seg = np.copy(seg)
+
+        has_seg = seg is not None
+
+        # apply transpose_forward, this also needs to be applied to the spacing!
+        data = data.transpose([0, *[i + 1 for i in plans_manager.transpose_forward]])
+        if seg is not None:
+            seg = seg.transpose([0, *[i + 1 for i in plans_manager.transpose_forward]])
+        original_spacing = [properties['spacing'][i] for i in plans_manager.transpose_forward]
+
+        # crop, remember to store size before cropping!
+        shape_before_cropping = data.shape[1:]
+        properties['shape_before_cropping'] = shape_before_cropping
+        # this command will generate a segmentation. This is important because of the nonzero mask which we may need
+        data, seg, bbox = crop_to_nonzero(data, seg)
+        properties['bbox_used_for_cropping'] = bbox
+        # print(data.shape, seg.shape)
+        properties['shape_after_cropping_and_before_resampling'] = data.shape[1:]
+
+        # resample
+        target_spacing = configuration_manager.spacing  # this should already be transposed
+
+        if len(target_spacing) < len(data.shape[1:]):
+            # target spacing for 2d has 2 entries but the data and original_spacing have three because everything is 3d
+            # in 2d configuration we do not change the spacing between slices
+            target_spacing = [original_spacing[0]] + target_spacing
+        new_shape = compute_new_shape(data.shape[1:], original_spacing, target_spacing)
+
+        # normalize
+        # normalization MUST happen before resampling or we get huge problems with resampled nonzero masks no
+        # longer fitting the images perfectly!
+        data = self._normalize(data, seg, configuration_manager,
+                               plans_manager.foreground_intensity_properties_per_channel)
+
+        # print('current shape', data.shape[1:], 'current_spacing', original_spacing,
+        #       '\ntarget shape', new_shape, 'target_spacing', target_spacing)
+        old_shape = data.shape[1:]
+        
+        # find inputs channels that have images and channels that have segmentations
+        scheme_list = configuration_manager.normalization_schemes
+        img_channels = []
+        mask_channels = []
+        for chanIdx in range(len(scheme_list)):
+            if scheme_list[chanIdx] == "NoNormalization":
+                mask_channels.append(chanIdx)
+            else:
+                img_channels.append(chanIdx)
+                
+        # split data into image channels and mask channels
+        data_img = data[img_channels, 1:]
+        data_mask = data[mask_channels, 1:]
+        
+        
+        # here, we split the data, resampling the normalized channels (MRI input) and masks (previous segmentations) separately
+        # this ensures we handle the masks correctly when preprocessing
+        
+        # use data preprocessor for data channel
+        data_img = configuration_manager.resampling_fn_data(data_img, new_shape, original_spacing, target_spacing)
+        
+        # use segmentation preprocessor for mask input channels
+        data_mask = configuration_manager.resampling_fn_seg(data_mask, new_shape, original_spacing, target_spacing)
+        
+        # segmentation preprocessor for output segmentation
+        seg = configuration_manager.resampling_fn_seg(seg, new_shape, original_spacing, target_spacing)
+        
+        # combine data and segmentation back into single array
+        tmp_data = np.zeros(data.shape)
+        tmp_data[img_channels, 1:] = data_img
+        tmp_data[mask_channels, 1:] = data_mask
+        data = tmp_data
+
+        if self.verbose:
+            print(f'old shape: {old_shape}, new_shape: {new_shape}, old_spacing: {original_spacing}, '
+                  f'new_spacing: {target_spacing}, fn_data: {configuration_manager.resampling_fn_data}')
+
+        # if we have a segmentation, sample foreground locations for oversampling and add those to properties
+        if has_seg:
+            # reinstantiating LabelManager for each case is not ideal. We could replace the dataset_json argument
+            # with a LabelManager Instance in this function because that's all its used for. Dunno what's better.
+            # LabelManager is pretty light computation-wise.
+            label_manager = plans_manager.get_label_manager(dataset_json)
+            collect_for_this = label_manager.foreground_regions if label_manager.has_regions \
+                else label_manager.foreground_labels
+
+            # when using the ignore label we want to sample only from annotated regions. Therefore we also need to
+            # collect samples uniformly from all classes (incl background)
+            if label_manager.has_ignore_label:
+                collect_for_this.append([-1] + label_manager.all_labels)
+
+            # no need to filter background in regions because it is already filtered in handle_labels
+            # print(all_labels, regions)
+            properties['class_locations'] = self._sample_foreground_locations(seg, collect_for_this,
+                                                                                   verbose=self.verbose)
+            seg = self.modify_seg_fn(seg, plans_manager, dataset_json, configuration_manager)
+        if np.max(seg) > 127:
+            seg = seg.astype(np.int16)
+        else:
+            seg = seg.astype(np.int8)
+        return data, seg, properties
+
+
+def example_test_case_preprocessing():
+    # (paths to files may need adaptations)
+    plans_file = '/home/isensee/drives/gpu_data/nnUNet_preprocessed/Dataset219_AMOS2022_postChallenge_task2/nnUNetPlans.json'
+    dataset_json_file = '/home/isensee/drives/gpu_data/nnUNet_preprocessed/Dataset219_AMOS2022_postChallenge_task2/dataset.json'
+    input_images = ['/home/isensee/drives/e132-rohdaten/nnUNetv2/Dataset219_AMOS2022_postChallenge_task2/imagesTr/amos_0600_0000.nii.gz', ]  # if you only have one channel, you still need a list: ['case000_0000.nii.gz']
+
+    configuration = '3d_fullres'
+    pp = DefaultPreprocessor()
+
+    # _ because this position would be the segmentation if seg_file was not None (training case)
+    # even if you have the segmentation, don't put the file there! You should always evaluate in the original
+    # resolution. What comes out of the preprocessor might have been resampled to some other image resolution (as
+    # specified by plans)
+    plans_manager = PlansManager(plans_file)
+    data, _, properties = pp.run_case(input_images, seg_file=None, plans_manager=plans_manager,
+                                      configuration_manager=plans_manager.get_configuration(configuration),
+                                      dataset_json=dataset_json_file)
+
+    # voila. Now plug data into your prediction function of choice. We of course recommend nnU-Net's default (TODO)
+    return data
+
+
+if __name__ == '__main__':
+    # example_test_case_preprocessing()
+    # pp = DefaultPreprocessor()
+    # pp.run(2, '2d', 'nnUNetPlans', 8)
+
+    ###########################################################################################################
+    # how to process a test cases? This is an example:
+    # example_test_case_preprocessing()
+    seg = SimpleITK.GetArrayFromImage(SimpleITK.ReadImage('/home/isensee/temp/H-mito-val-v2.nii.gz'))[None]
+    DefaultPreprocessor._sample_foreground_locations(seg, np.arange(1, np.max(seg) + 1))
