5682c334beae4b559db1979d10cf1d000827c7a6
borkild <ben.orkild@utah.edu>
2026-05-04 16:52:53 -0600

    acutally need checkpoints

--- nnunetv2/training/semiSupervised_functions/setup_for_iteration1.py
+++ nnunetv2/training/semiSupervised_functions/setup_for_iteration1.py
@@ -33,7 +33,7 @@ def setup_iteration1(nnUnet_raw_dataset_path: str, nnUnet_preprocessed_dataset_p
     shutil.copytree(nnUnet_preprocessed_dataset_path, dataset_pp, ignore=shutil.ignore_patterns("iterations"))
     print("copying results files")
     shutil.copytree(nnUnet_results_dataset_path, dataset_res, 
-                    ignore=shutil.ignore_patterns("iterations", "train", "validation", "*Plots", "*.xlsx", "tstInference", "*.txt", "*.png", "*.pth"))
+                    ignore=shutil.ignore_patterns("iterations", "train", "validation", "*Plots", "*.xlsx", "tstInference", "*.txt", "*.png"))
     
     # here we also set up our temporary directory for storing predictions on unlabeled data after each fold
     if not os.path.isdir( os.path.join(nnUnet_results_dataset_path, "tmp_outputs") ):
